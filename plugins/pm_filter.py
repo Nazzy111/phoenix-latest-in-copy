51795231b86efbda1420b559fc8a049df4bdcd2c
Nasim <107646990+Nazzy111@users.noreply.github.com>
2022-07-12 16:20:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -414,13 +414,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer()
     elif query.data == "start":
         buttons = [[
-            InlineKeyboardButton("➕️ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕️", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+            InlineKeyboardButton("🔰 𝘈𝘋𝘋 𝘔𝘌 𝘛𝘖 𝘠𝘖𝘜𝘙 𝘎𝘙𝘖𝘜𝘗 🔰", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
-            InlineKeyboardButton("⭐️ Owner ⭐️", url="https://t.me/NJaNu_yir"), 
-            InlineKeyboardButton("📢 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 📢", url="https://t.me/MWMFilms")
+            InlineKeyboardButton("⚡ 𝐎𝐖𝐍𝐄𝐑 ⚡", url="https://t.me/NJaNu_yir"), 
+            InlineKeyboardButton("♻️ 𝐔𝐏𝐃𝐀𝐓𝐄𝐒 ♻️", url="https://t.me/MWMFilms")
             ],[      
-            InlineKeyboardButton("ℹ️ 𝙷𝙴𝙻𝙿 ℹ️", callback_data="help"),
-            InlineKeyboardButton("💫 𝙰𝙱𝙾𝚄𝚃 💫", callback_data="about")
+            InlineKeyboardButton("ℹ️ 𝐇𝐄𝐋𝐏 ℹ️", callback_data="help"),
+            InlineKeyboardButton("🌚 𝐀𝐁𝐎𝐔𝐓 🌝", callback_data="about")
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
