77bd55acdb4c9e65f4a946eeed6763bd8d4d8f5b
Nasim <107646990+Nazzy111@users.noreply.github.com>
2022-07-14 12:54:08 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,8 +37,8 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('ℹ️ʜᴇʟᴘ ', url=f"https://t.me/{temp.U_NAME}?start=help"),
+            InlineKeyboardButton('📢 ᴜᴘᴅᴀᴛᴇs', url='https://t.me/nazzymovies1')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
